7683421d474de7ee41a829eca54274d6f9c6d9b5
peshopbs2 <peshopbs2@gmail.com>
2025-04-22 11:43:04 +0300

    Update des_encrypt.c

--- des_encrypt.c
+++ des_encrypt.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-#include <openssl/des.h>
 #include <stdlib.h>
+#include <openssl/des.h>
 #define KEY_SIZE 8
-#define MAXN 100
+#define MAXN 1000
 
 int main()
 {
     unsigned char key[KEY_SIZE] = "banichka";
     unsigned char input[MAXN];
 
-    fgets(input, MAXN, stdin);
-    input[strcspn(input, "\n")] = '\0';
+    scanf("%s", input);
 
     unsigned int length = strlen(input);
 
@@ -19,15 +18,17 @@ int main()
     unsigned char padded_input[padded_length];
     unsigned char output[padded_length];
 
+    int difference_length = padded_length - length;
     memcpy(padded_input, input, length);
-    memset(padded_input + length, padded_length - length, padded_length - length);
+    memset(padded_input + length, difference_length, difference_length);
 
     DES_key_schedule key_schedule;
     DES_set_key( (DES_cblock*) key, &key_schedule);
+
     for(int i = 0; i < padded_length; i += 8)
     {
-        DES_ecb_encrypt((DES_cblock*) (padded_input + i), (DES_cblock*) (output + i),
-            &key_schedule, DES_ENCRYPT);
+        DES_ecb_encrypt( (DES_cblock*) (padded_input + i),
+            (DES_cblock*) (output + i), &key_schedule, DES_ENCRYPT);
     }
 
     for(int i = 0; i < padded_length; i++)
@@ -35,5 +36,4 @@ int main()
         printf("%02x", output[i]);
     }
     return EXIT_SUCCESS;
-    //gcc des_encrypt.c -o des_encrypt -lcrypto
 }
